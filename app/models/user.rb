17e6614fcf17139fe7d670a108ae34916ba269ca
Travis Tillotson <travistillotson@traviss-MBP.attlocal.net>
2023-02-20 21:36:00 -0600

    style landing page and devise views

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  after_create :create_default_list
+
+  def create_default_list
+  end
+end
