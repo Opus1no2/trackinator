25aa36b244a53e997f717005fcc2cf8e32e981ff
Opus1no2 <tillotson.travis@gmail.com>
2023-02-25 10:31:58 -0600

    fix frozen string bug using + instead of <<

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -3,7 +3,7 @@
 module ListsHelper
   def list_classes(list_id, params_id)
     classes = 'mb-2 flex space-between align-center'
-    classes << ' active' if list_id == params_id.to_i
+    classes + ' active' if list_id == params_id.to_i
     classes
   end
 end
