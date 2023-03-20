ba28112a26ec69cbb8950151acd6a8504022306f
Opus1no2 <tillotson.travis@gmail.com>
2023-03-19 20:07:23 -0500

    use view logic to show finished list state instead of js. small ui improvements

--- app/javascript/controllers/item_controller.js
+++ app/javascript/controllers/item_controller.js
@@ -3,12 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     this.layout();
-
-    const list_items = this.element.querySelectorAll('.list-item-row');
-
-    if (!list_items.length) {
-      this.element.querySelector('[data-id="finished-state"]').classList.toggle('hidden');
-    }
   }
   layout() {
     const container = document.querySelector('[data-id="list-items-cont"]');
