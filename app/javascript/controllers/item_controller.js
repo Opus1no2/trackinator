8645701ad0139bd71ebb58b35108ba80dffa06b9
Opus1no2 <tillotson.travis@gmail.com>
2023-10-13 19:54:48 -0500

    better mobile styling

--- app/javascript/controllers/item_controller.js
+++ app/javascript/controllers/item_controller.js
@@ -2,11 +2,5 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.layout();
-  }
-  layout() {
-    const container = document.querySelector('[data-id="list-items-cont"]');
-    const newItemInput = document.querySelector('[data-id="list-item-input"]');
-    newItemInput.style.width = `${container.clientWidth}px`;
   }
 }
