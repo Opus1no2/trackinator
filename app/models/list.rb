bf3ff8a1ee09ce8b77ad5ec1bd7b13e3977f5f6d
Opus1no2 <tillotson.travis@gmail.com>
2023-10-15 15:43:01 -0500

    add lists on mobile

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,7 +6,11 @@ class List < ApplicationRecord
   has_many :list_items, dependent: :destroy
   has_many :list_view_settings, dependent: :destroy
 
-  after_create :create_list_settings
+  after_create :create_list_settings, :create_list_items
+
+  def create_list_items
+    list_items.create!(description: 'My First Item')
+  end
 
   def create_list_settings
     list_view_settings.create!
